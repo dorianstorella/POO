1081f5119f5b0e875dfeab8ee065729ed4fc756b
Dorian Storella <dorianstorella@gmail.com>
2020-12-08 13:59:14 +0100

    ajout premier exercice

---
+++ POO-etape1/index.php
@@ -0,0 +1,37 @@
+<form action="">
+<input type="text" name="" id="">
+<select name="" id=""></select>
+<button type="submit"></button>
+<textarea name="" id="" cols="30" rows="10"></textarea>
+<input type="radio" name="" id="">
+<input type="checkbox">
+</form>
+<?php
+class Form{
+    public $inputText;
+    function getinputText(){
+
+    }
+    public $select;
+    function getselect(){
+
+    }
+    public $button; 
+    function getbutton(){
+
+    }
+    public $textarea;
+    function gettextarea(){
+
+    }
+    public $inputRadio;
+    function getinputRadio(){
+
+    }
+    public $inputCheckbox
+    function getinputCheckbox(){
+        
+    }
+} 
+
+?>
