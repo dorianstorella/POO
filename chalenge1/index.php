1e40efa899ff6b6d0015b7f065ad034bba98658c
Dorian Storella <dorianstorella@gmail.com>
2020-12-08 16:38:05 +0100

    durdur la vie d artiste

--- chalenge1/index.php
+++ chalenge1/index.php
@@ -1,5 +1,5 @@
 <?php 
-$servername = "localhost";
+/*$servername = "localhost";
 $username = "root";
 $password = "";
 
@@ -7,25 +7,28 @@ $password = "";
 try {
     $db = new PDO("mysql:host=$servername;dbname=poo;charset=utf8", $username, $password);
     //setting the PDO error mod to exception
- /* $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "CREATE DATABASE POO";
     //using exec() because noresul are returned
     $db ->exec($sql);
-    echo "database created successfulle whith the name POO";*/
+    echo "database created successfulle whith the name POO";
 } 
 catch(PDOExeption $e){
     echo $sql . "
     ". $e->getMessage();
 }
 $db=null;
-
+*/
 
 
 class ConnPDO{
-    public $servername ="localhost";
-    public $datadase="poo"
+    public $servername ="localhost";    // propriete
+    public $datadase="poo";
     public $username ="root";
-    public $password="";    
+    public $password="";
+       
+    function connection($servername,$datadase,$usernamer,$password){ //methode  
+        $db = new PDO("mysql:host=$this->$servername;dbname=poo;charset=utf8", $this->$username, $this->$password);  
 }
-
+}
 ?>
