ea8894d176860f330d1174ad2f7e8ced463c9a54
Dorian Storella <dorianstorella@gmail.com>
2020-12-08 15:55:11 +0100

    ajout premier exercice

---
+++ chalenge1/index.php
@@ -0,0 +1,31 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+
+try {
+    $db = new PDO("mysql:host=$servername;dbname=poo;charset=utf8", $username, $password);
+    //setting the PDO error mod to exception
+ /* $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE POO";
+    //using exec() because noresul are returned
+    $db ->exec($sql);
+    echo "database created successfulle whith the name POO";*/
+} 
+catch(PDOExeption $e){
+    echo $sql . "
+    ". $e->getMessage();
+}
+$db=null;
+
+
+
+class ConnPDO{
+    public $servername ="localhost";
+    public $datadase="poo"
+    public $username ="root";
+    public $password="";    
+}
+
+?>
