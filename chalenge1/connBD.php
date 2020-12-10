ffe20e29bef86cadf53a9166a7503d8db9547f61
Dorian Storella <dorianstorella@gmail.com>
2020-12-10 14:18:21 +0100

    chalenge 1

--- chalenge1/connBD.php
+++ chalenge1/connBD.php
@@ -1,19 +1,25 @@
 <?php
 class connexion
 {
-    private $db;   //propiété
+    private $conn;   //propiété
     public function __construct($dbname,$user, $pass)
     {
         try
        {
-            $this->db = new PDO("mysql:host=localhost;dbname=$dbname;charset=utf8", $user, $pass);
+            $this->conn = new PDO("mysql:host=localhost;dbname=$dbname;charset=utf8", $user, $pass);
         }
         catch(Exception $e)
         {
             die('Error : '.$e->getMessage());
         }
     }
-    
+    public function countTable($dbQUERY)
+    {
+        $stmt = $this->conn->prepare($dbQUERY);
+        $stmt->exec();
+        return $stmt->rowCount();
+
+    }
    
 
 }
