ff64f40f45431dbd6dca6c043b5c45d796c744c9
Dorian Storella <dorianstorella@gmail.com>
2020-12-11 14:55:46 +0100

    presque fini

--- chalenge2/coDB.php
+++ chalenge2/coDB.php
@@ -1,14 +1,12 @@
 <?php 
 class Connection 
 {
-    private $dbname;
-    private $user;
-    private $pass;  
-    public static function con($dbname, $user , $pass)
+    private $db;
+    public function __construct($dbname, $user , $pass)
     {
         try
        {
-           $connect = new PDO("mysql:host=localhost;dbname=$dbname;charset=utf8", $user, $pass);
+           $this->db= new PDO("mysql:host=localhost;dbname=$dbname;charset=utf8", $user, $pass);
             echo 'co reussie';
         }
         catch(Exception $e)
@@ -16,38 +14,68 @@ class Connection
             die('Error : '.$e->getMessage());
         }
     }
-    
+        static function connect($dbname, $user,$pass)
+        {
+            return new PDO("mysql:host=localhost;dbname=$dbname;charset=utf8", $user, $pass);
+        }
 }
  
 
 class Post 
 {
-    private $id_post;
+    private $id_post=null;
     private $title_post;
     private $content_post;
     
-    
+    public function __construct($id,$title,$content)
+    {
+        $this->id_post = $id;
+        $this->tittle_post = $title;
+        $this->content_post = $content;
+    }
+
     public function addPost()
     {   
-      
-        
-       
+        $connect = Connection::connect("poo","root","");
+        $stmt = $connect->prepare("INSERT INTO clients VALUES (NULL, :post, :content)");
+        $stmt->bindParam(':post', $this->title_post);
+        echo $stmt->execute();
     }
 
     public function removePoste()
     {
-
-    }
-
-    public function findAllPost($id_post)
-    {
-        $co = Connection::con("becode", "root" , ""); 
-       
-        $stmt= $co->prepare("SELECT (possesseur) FROM (jeux_video)");
+        $connect = Connection::connect("poo","root","");
+        $stmt = $connect->prepare("DELETE FROM clients WHERE id_post=".$id);
         $stmt->execute();
     }
-    function getid()
+
+    public function findAllPost()
+    {
+        $allPost=[];
+        $connect = Connection::connect("becode", "root" , ""); 
+        $stmt= $connect->prepare("SELECT * FROM clients");
+        $stmt->execute();
+        while($fetch = $stmt->fetch())
+        {
+            $allPost[] = new Post($fetch['id_post'],$fetch['title_post'],$fetch['content_post']);
+        }
+        return $allPost;
+    }
+    
+    function getId()
     {
         return $this->stmt;
+    }
+    
+
+    function getTitle()
+    {
+        return $this->title_post;
+    }
+    
+
+    function getContent()
+    {
+        return $this->content_post;  
     }
 }
--- chalenge2/index.php
+++ chalenge2/index.php
@@ -4,5 +4,54 @@ require 'coDB.php';
 $co->con("becode","root","");
 */
 
-$new = new Post("becode","root","");
-$new-> findallpost("possesseur");
+if (isset($_POST['title'],$_POST['content'])){
+    $onePost = new Post(null,$_POST['title'],$_POST['content']);
+    $onePost->addPost();
+}
+if ($_POST['submitDelete']=='delete'){
+    foreach($_POST['delete'] as $post){
+        Post::removePost($post);
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<h1>Add post</h1>
+	<form action="" method="POST">
+        <label for="title">Title:</label>
+        <input type="text" name="title">
+        <label for="content">Content:</label>
+        <textarea name="content"></textarea>
+        <button type="submit" name="button">Submit</button>
+	</form>
+
+
+    <table>
+        <thead>
+            <tr>
+                <th style="border: 1px outset">id</th>
+                <th style="border: 1px outset">title</th>
+                <th style="border: 1px outset">content</th>
+            </tr>
+        </thead>
+        <tbody>
+<?php
+        echo '<form method="post">';
+            foreach(Post::findAllPost() as $post){
+                echo '<tr>';
+                    echo '<td style="border: 1px solid black">'.$post->getId().'</td>';
+                    echo '<td style="border: 1px solid black">'.$post->getTitle().'</td>';
+                    echo '<td style="border: 1px solid black">'.$post->getContent().'</td>';
+                    echo '<td style="border: 1px solid black"><input type="checkbox" value="'.$post->getId().'" name="delete[]"></td>';
+                echo '</tr>';
+            }
+            echo '<input class="button" type="submit" name="submitDelete" value="delete">';
+        echo '</form>';
+?>
+        </tbody>
+    </table>
+</body>
+</html>
